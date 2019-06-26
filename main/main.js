063559b66a9422f8648cfcfd5460eb5af44b0226
aha <ntuaha@gmail.com>
2019-06-26 10:22:05 +0800

    working

--- main/main.js
+++ main/main.js
@@ -1,1 +1,10 @@
-console.log("HI")
+// https://vuejs.org/v2/guide/
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: `專長於組裝各式語言與各大平台服務， 打造最小可行產品原型． 曾獲得2011 政府開放資料平台 App 社會組首獎． 2015 PIXNET
+        Mobile Service
+        社會組首獎． 2014 DSC R
+        課程講師． 2017 pycon 與2017 DSC 講者。`
+    }
+})
